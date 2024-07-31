ee403b0642bc4731ecbbf1af6a7cfc40b705f6bb
Thomas King <thomasgking42@gmail.com>
2024-07-31 14:22:05 +0100

    Final version Story 2 - validation form inputs

--- form.php
+++ form.php
@@ -7,19 +7,34 @@ require_once 'db.php';
     <title>Trainer Tracker</title>
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Rubik+Mono+One&display=swap" rel="stylesheet">
+
 </head>
 <body>
 <nav>
-    <h1>Trainer Tracker</h1>
-    <h3>A virtual place to store your shoes...</h3>
+        <h1>Trainer Tracker</h1>
+        <h3>A virtual room to store your shoes...</h3>
 </nav>
 <form method="post" class="box">
         <a href="index.php">ⅹ</a>
         <h4>Add shoe to your collection: </h4>
     <div class="label">
         <label for="brandID">Brand: <p class="required">required</p></label>
-        <input required id="brandID" name="brand" type="text" placeholder="e.g. Adidas">
+        <select required id="brandID" name="brand">
+            <option disabled value="select">Please Select</option>
+            <option value="adidas">Adidas</option>
+            <option value="nike">Nike</option>
+            <option value="reebok">Reebok</option>
+            <option value="vans">Vans</option>
+            <option value="converse">Converse</option>
+            <option value="puma">Puma</option>
+            <option value="newBalance">New Balance</option>
+            <option value="asics">Asics</option>
+            <option value="solomon">Solomon</option>
+            <option value="fila">Fila</option>
+            <option value="skechers">Skechers</option>
+            <option value=other"">Other</option>
+        </select>
     </div>
     <br>
     <div>
@@ -33,8 +48,8 @@ require_once 'db.php';
     </div>
     <br>
     <div>
-        <label for="costID">Cost (£): <p class="required">required</p></label>
-        <input required id="costID" name="cost" type="number" placeholder="e.g. 80">
+        <label for="costID">Cost (£): </label>
+        <input id="costID" name="cost" type="number" placeholder="e.g. 80">
     </div>
     <br>
     <div>
@@ -54,11 +69,41 @@ require_once 'db.php';
 
 if (isset($_POST['brand']) && isset($_POST['name']) && isset($_POST['colour']) && isset($_POST['cost']) && isset($_POST['image']))
 {
-    $brand = $_POST['brand'];
-    $name = $_POST['name'];
-    $colour = $_POST['colour'];
-    $cost = $_POST['cost'];
-    $image = $_POST['image'];
+
+        if ($_POST['brand'] !== 'select')
+        {
+            $brand = $_POST['brand'];
+        }
+
+        if (gettype($_POST['name']) === 'string')
+        {
+            $name = $_POST['name'];
+        }
+
+        if (gettype($_POST['colour']) === 'string')
+        {
+            $colour = $_POST['colour'];
+        }
+
+        if ($_POST['cost'] === '')
+        {
+            $_POST['cost'] = null;
+        }
+        else
+        {
+            $cost = $_POST['cost'];
+        }
+
+        $regex = '/\.(png|jpeg|jpg)$/i';
+
+        if (gettype($_POST['image']) === 'string' && preg_match($regex ,$_POST['image']))
+        {
+            $image = $_POST['image'];
+        }
+        else
+        {
+            $image = 'placeholder.png';
+        }
 
     $query = $db->prepare("INSERT INTO `shoes` (`brand`, `name`, `colour`, `cost`, `image`) VALUES (:brand, :name, :colour, :cost, :image);");
 
--- index.php
+++ index.php
@@ -3,14 +3,22 @@
     <title>Trainer Tracker</title>
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Rubik+Mono+One&display=swap" rel="stylesheet">
+
 </head>
 <body>
 <nav>
-    <h1>Trainer Tracker</h1>
-    <h3>A virtual place to store your shoes...</h3>
+    <div>
+        <h1>Trainer Tracker</h1>
+        <h3>A virtual room to store your shoes...</h3>
+    </div>
+<!--    <div>-->
+<!--        <form>-->
+<!--            <input id="searchID" name="search" type="text" placeholder="Search">-->
+<!--        </form>-->
+<!--    </div>-->
 </nav>
-
+<main>
 <?php
 
 require_once 'db.php';
@@ -23,11 +31,26 @@ require_once 'db.php';
         echo '<hr>';
         echo "<h4 class='brand'>" . $shoe['brand'] . ' ~ '.$shoe['name'] . "</h4>";
         echo "<h4>Colour: " . $shoe['colour'] . "</h4>";
-        echo "<h4>Price: £" . $shoe['cost'] . "</h4>";
+        echo "<h4>Price: ";
+        if ($shoe['cost'] == NULL)
+        {
+            echo "N/A";
+        }
+        else
+        {
+            echo "£" . $shoe['cost'];
+        }
+
+        echo "</h4>";
         echo "</div>";
     }
         echo "<div class=box> <a href='form.php'><h1>Add<br>Shoe<br>+</h1></a></div>";
-?>
 
+//$search = $_POST['search'];
+//$query = $db->prepare("SELECT `brand`, `name`, `colour`, `cost` FROM `shoes` WHERE `name` LIKE :search OR `colour` LIKE :search OR `brand` LIKE :search OR `colour` LIKE :search");
+//$query->bindParam(':search', $search);
+//echo "$search"
+?>
+</main>
 </body>
 </html>
