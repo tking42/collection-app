dd32ea99fa195275d8b67726b26558b71ed7d271
Thomas King <thomasgking42@gmail.com>
2024-08-01 14:45:57 +0100

    Story 4- brand filter

--- db.php
+++ db.php
@@ -3,20 +3,3 @@
 $db = new PDO('mysql:host=db; dbname=shoes', 'root', 'password');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-    if (isset($_POST['sort']) && $_POST['sort'] == 'costSort')
-    {
-        $query = $db->prepare("SELECT `brand`, `name`, `colour`, `cost`, `image` FROM `shoes` ORDER BY `cost`");
-    }
-    elseif (isset($_POST['sort']) && $_POST['sort'] == 'brandSort')
-    {
-        $query = $db->prepare("SELECT `brand`, `name`, `colour`, `cost`, `image` FROM `shoes` ORDER BY `brand`, `name`");
-    }
-    else
-    {
-        $query = $db->prepare("SELECT `brand`, `name`, `colour`, `cost`, `image` FROM `shoes`");
-    }
-
-
-    $query->execute();
-    $results = $query->fetchAll();
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html lang="en">
 <head>
     <title>Trainer Tracker</title>
+<!--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />-->
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Rubik+Mono+One&display=swap" rel="stylesheet">
@@ -16,19 +17,46 @@
 <main>
     <div class="sort">
         <form method="post" action="index.php">
-            <label for="sortID">Sort By: </label>
-            <select required id="sortID" name="sort">
-                <option value="noSort">Please Select</option>
-                <option value="brandSort">Brand A-Z</option>
-                <option value="costSort">Cost low-high</option>
-            </select>
-            <input type="submit" value="Go!">
+            <div>
+                    <label for="sortID">Sort: </label>
+                    <select required id="sortID" name="sort">
+                        <option value="noSort">Please Select</option>
+                        <option value="brandSort">Brand A-Z</option>
+                        <option value="dscBrandSort">Brand Z-A</option>
+                        <option value="costSort">Cost low-high</option>
+                        <option value="dscCostSort">Cost high-low</option>
+                    </select>
+                    <input type="submit" value="Go!">
+            </div>
+        </form>
+
+        <form method="post" action="index.php">
+            <div>
+                <label for="filterID">Brand filter: </label>
+                <select required id="filterID" name="brandFilter">
+                    <option value="noSort">All</option>
+                    <option value="adidas">Adidas</option>
+                    <option value="nike">Nike</option>
+                    <option value="reebok">Reebok</option>
+                    <option value="vans">Vans</option>
+                    <option value="converse">Converse</option>
+                    <option value="puma">Puma</option>
+                    <option value="newBalance">New Balance</option>
+                    <option value="asics">Asics</option>
+                    <option value="solomon">Solomon</option>
+                    <option value="fila">Fila</option>
+                    <option value="skechers">Skechers</option>
+                    <option value=other"">Other</option>
+                </select>
+                <input type="submit" value="Go!">
+            </div>
         </form>
     </div>
 
+
 <?php
 
-require_once 'db.php';
+require_once 'sort.php';
 
     echo "<div class=container>";
     foreach ($results as $shoe)
